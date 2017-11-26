fc791ea0af1527fcecc5b86fda16497949e4cbf2
Alina Kalyakina <s02160070@stud.cs.msu.ru>
2017-11-26 19:02:13 +0300

    Some more tests are run now

--- scripts/testercov.sh
+++ scripts/testercov.sh
@@ -7,17 +7,29 @@ TESTS=../tests/
 TANDT=../src/TandT.c
 	mkdir -p $OUT
     cd $OUT
-    gcc $SRC/test.c  $SRC/TandT1.c -I $INC -o ./test.out -Wall --coverage -std=gnu11
-    gcc $SRC/tester.c $SRC/TandT2.c -I $INC -o ./tester.out -Wall --coverage -std=gnu11
+    gcc $SRC/TandT.c -I $INC -c -o./TandT.o --coverage -std=gnu11
+    for VAR in test testlongq tester testerbad1 testerbad2 testerbad3
+    do
+    gcc $SRC/$VAR.c  TandT.o -I $INC -o ./$VAR.out -Wall --coverage -std=gnu11
+    done
     echo First test
 	./tester.out ./test.out <$TESTS/test1; echo 
     echo Second test
     ./tester.out ./test.out <$TESTS/test2; echo
     echo Third test
     ./tester.out ./test.out; echo
+    echo Fourth test without argument
+    ./tester.out; echo
+    echo Firth test long question
+    ./tester.out ./testlongq.out; echo
+    echo Sixth test 
+    ./tester.out ./test.out; echo
+    echo Test bad que in question request
+    ./testerbad1.out ./test.out; echo
+    echo Test bad instruction
+    ./testerbad2.out ./test.out; echo
+    echo Test bad que in check request
+    ./testerbad3.out ./test.out; echo
 	git clone https://github.com/linux-test-project/lcov.git
-    for VAR in tester test TandT1 TandT2
-    do
-    $LCOV/lcov --directory ./$VAR.gcda --capture --output-file $VAR.info --rc lcov_branch_coverage=1
-    $LCOV/genhtml -o $VAR.html $VAR.info --branch-coverage
-    done
+    lcov --directory ./ --capture --output-file index.info --rc lcov_branch_coverage=1
+    genhtml -o index.html index.info --branch-coverage
